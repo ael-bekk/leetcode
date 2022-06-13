4467b397b1aede4085a084083ba85fa1c6d4c4a0
abdellah el bekkali <84334734+ael-bekk@users.noreply.github.com>
2022-06-13 19:56:12 +0100

    Time: 43 ms (25.00%), Space: 16.8 MB (71.25%) - LeetHub

---
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.c
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+int countNodes(struct TreeNode* root)
+{
+    struct TreeNode *q = root, *p = root;
+    int l = 0, r = 0;
+    if (!root)
+        return (0);
+    
+    while (q || p)
+    {
+        if (q)
+        {
+            q = q->left;
+            l++;
+        }
+        if (p)
+        {
+            p = p->right;
+            r++;
+        }
+    }
+    if (l == r)
+        return ((1 << l) - 1);
+    return (1 + countNodes(root->left) + countNodes(root->right));
+}
