3f72ea05b0223d199d646e3d46bff4728d2d801e
abdellah el bekkali <84334734+ael-bekk@users.noreply.github.com>
2022-06-13 11:21:13 +0100

    Time: 276 ms (9.09%), Space: 13.2 MB (45.45%) - LeetHub

---
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.c
@@ -0,0 +1,23 @@
+
+
+int maximumUniqueSubarray(int* nums, int n)
+{
+    int i = -1, l = 0, r = 0, sum = 0, max = 0;
+    int arr[10001] = {0};
+
+    while (++i < n)
+        if (!arr[nums[i]])
+        {
+            arr[nums[i]] = 1,
+            sum += nums[i],
+            max = (max > sum) ? max : sum,
+            r++;
+        }
+        else if (l < r)
+        {
+            sum -= nums[l],
+            arr[nums[l++]] = 0,
+            i--;
+        }
+    return (max);
+}
