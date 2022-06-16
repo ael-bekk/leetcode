d83fc48eca203cbd9acf6ccb5dfd0b852d2863db
abdellah el bekkali <84334734+ael-bekk@users.noreply.github.com>
2022-06-16 12:20:35 +0100

    Time: 10 ms (8.17%), Space: 6.1 MB (10.63%) - LeetHub

---
+++ 162-find-peak-element/162-find-peak-element.c
@@ -0,0 +1,24 @@
+
+
+int findPeakElement(int* nums, int numsSize)
+{
+    int l = 0, r = numsSize - 1, mid;
+
+    if (numsSize == 1)
+        return (0);
+    if (numsSize == 2)
+        return (nums[1] > nums[0]);
+    while (l < r)
+    {
+        mid = (l + r + 1) / 2;
+        if (mid && mid + 1 < numsSize && nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1])
+            return (mid);
+        if (mid && nums[mid] < nums[mid - 1])
+            r = mid - 1;
+        else if (mid + 1 < numsSize && nums[mid] < nums[mid + 1])
+            l = mid + 1;
+        else if (!mid || mid + 1 == numsSize)
+            return (mid); 
+    }
+    return (l);
+}
