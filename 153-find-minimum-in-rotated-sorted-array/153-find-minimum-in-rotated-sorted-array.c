f04e7924a985e108d7748bfeb214a59e78790602
abdellah el bekkali <84334734+ael-bekk@users.noreply.github.com>
2022-06-18 15:02:34 +0100

    Time: 8 ms (16.55%), Space: 5.9 MB (93.38%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.c
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.c
@@ -2,15 +2,13 @@
 
 int findMin(int* nums, int numsSize){
     int l = 0, r = numsSize - 1, mid;
-    while (l < r)
+    while (l + 1 < r)
     {
         mid = (l + r) / 2;
-        if (r - l == 1)
-            return ((nums[r] > nums[l]) ? nums[l] : nums[r]);
         if (nums[mid] > nums[l] && nums[mid] > nums[r])
             l = mid;
         else
             r = mid;    
     }
-    return (nums[r]);
+    return ((nums[r] > nums[l]) ? nums[l] : nums[r]);
 }
