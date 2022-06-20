c8b3609af999c1f845ddc395cff05d9977ac4190
abdellah el bekkali <84334734+ael-bekk@users.noreply.github.com>
2022-06-20 21:55:26 +0100

    Time: 288 ms (43.81%), Space: 25.9 MB (38.26%) - LeetHub

---
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.c
@@ -0,0 +1,8 @@
+
+
+int findCenter(int** edges, int edgesSize, int* edgesColSize)
+{
+    if (edges[0][0] == edges[1][1] || edges[0][0] == edges[1][0])
+        return (edges[0][0]);
+     return (edges[0][1]);
+}
